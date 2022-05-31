c03c90a0558f4887786986ec25eecf3fee8589a4
Sheetal S <56045222+sheetal104@users.noreply.github.com>
2022-05-31 18:04:36 +0530

    Add files via upload

---
+++ flask/ProphetAPI.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+%matplotlib inline
+import matplotlib.pyplot as plt
+import csv
+import random
+import re
+import time
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from matplotlib.style import use
+import numpy as np
+from fbprophet import Prophet
+import pickle
+
+class ProphetClass:
+    def __init__():
+        pass
+    
+    def preprocess(df):
+        dataset = df
+        df_User1 = dataset[dataset['UserID']==1]
+        df = df_User1
+        df = df.rename(columns={'Usage': 'y', 'Time': 'ds'})
+        df.drop('UserID', inplace=True, axis=1)
+        df['ds']= to_datetime(df['ds'])
+        return df
+
+    def train(df):
+        test_size = 50
+        test_ind = len(df) - test_size
+        train = df.iloc[:test_ind]
+        test = df.iloc[test_ind:]
+
+        prophet_model = Prophet()
+        prophet_model.fit(train)
+        with open('prophet.pkl', 'wb') as pkl:
+            pickle.dump(prophet_model, pkl)
+        return test
+    
+    def predict(test):
+        test_size = 50
+        with open('prophet.pkl', 'rb') as pkl:
+            prophet_model = pickle.load(pkl)
+            future = test['ds']
+            future = pd.DataFrame(future)
+            future.columns = ['ds']
+            future['ds']= to_datetime(future['ds'])
+            prediction = prophet_model.predict(future)
+            prediction = prediction[['yhat','ds']]
+            prediction.rename(columns = {'ds':'Time', 'yhat':'forecast'}, inplace = True)
+            prediction.set_index('Time', inplace=True)
+            return prediction
+
+    def update(df):
+        with open('prophet.pkl', 'wb') as pkl:
+            prophet_model = pickle.load(pkl).update(df)
+            pickle.dump(prophet_model, pkl)
+
+
+  
