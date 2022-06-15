b176870a38aab845b1f39315d1f30647f420ee39
SujayAmbekar <ambekar.sujay@gmail.com>
2022-06-15 12:25:49 +0530

    LR model updated

--- flask/linearRegression.py
+++ flask/linearRegression.py
@@ -1,45 +1,98 @@
 import pandas as pd
 
 class linearRegressionClass:
-    def __init__():
-        pass 
+	def __init__():
+		pass 
 
-    def model(dataset):
-        # df=pd.DataFrame()
-        # df = pd.read_csv(dataset,index_col='Time',parse_dates=True)
-        df = dataset 
-        df.index = df['Time']
-        df.index.freq = 'MS'
-        #df.tail()
-        # df.columns = ['Usage']
-        df.plot(figsize=(12,8))
+	def preprocess(dataset):   
+		# df=pd.DataFrame()
+		# df = pd.read_csv(dataset,index_col='Time',parse_dates=True)
+		df = dataset 
+		df.index = df['Time']
+		df.index.freq = 'MS'
+		#df.tail()
+		# df.columns = ['Usage']
+		df.plot(figsize=(12,8))
 
-        df['Usage_LastMonth']=df['Usage'].shift(+1)
-        df['Usage_2Monthsback']=df['Usage'].shift(+2)
-        df['Usage_3Monthsback']=df['Usage'].shift(+3)
+		df['Usage_LastMonth']=df['Usage'].shift(+1)
+		df['Usage_2Monthsback']=df['Usage'].shift(+2)
+		df['Usage_3Monthsback']=df['Usage'].shift(+3)
         #df
+		
+		df=df.dropna()
+		#df
+		return df
 
-        df=df.dropna()
-        #df
+	def train(df):    
+		from sklearn.linear_model import LinearRegression
+		lin_model=LinearRegression()
 
-        from sklearn.linear_model import LinearRegression
-        lin_model=LinearRegression()
+		import numpy as np
+		x1,x2,x3,y=df['Usage_LastMonth'],df['Usage_2Monthsback'],df['Usage_3Monthsback'],df['Usage']
+		x1,x2,x3,y=np.array(x1),np.array(x2),np.array(x3),np.array(y)
+		x1,x2,x3,y=x1.reshape(-1,1),x2.reshape(-1,1),x3.reshape(-1,1),y.reshape(-1,1)
+		final_x=np.concatenate((x1,x2,x3),axis=1)
 
-        import numpy as np
-        x1,x2,x3,y=df['Usage_LastMonth'],df['Usage_2Monthsback'],df['Usage_3Monthsback'],df['Usage']
-        x1,x2,x3,y=np.array(x1),np.array(x2),np.array(x3),np.array(y)
-        x1,x2,x3,y=x1.reshape(-1,1),x2.reshape(-1,1),x3.reshape(-1,1),y.reshape(-1,1)
-        final_x=np.concatenate((x1,x2,x3),axis=1)
+		test_ind = len(df) - 30
+		train = df.iloc[:test_ind]
+		test = df.iloc[test_ind:]
+		X_train,X_test,y_train,y_test=final_x[:-30],final_x[-30:],y[:-30],y[-30:]
 
-        test_ind = len(df) - 30
-        train = df.iloc[:test_ind]
-        test = df.iloc[test_ind:]
-        X_train,X_test,y_train,y_test=final_x[:-30],final_x[-30:],y[:-30],y[-30:]
+		lin_model.fit(X_train,y_train) 
+		with open('LinearRegression.pkl', 'wb') as pkl:
+		    pickle.dump(lin_model, pkl)
+		return X_test 
 
-        lin_model.fit(X_train,y_train)  
+	def predict(test):
+		test_size = len(test)
+		with open('LinearRegression.pkl', 'rb') as pkl:
+			prediction = pd.DataFrame(pickle.load(pkl).predict(n_periods=test_size),index=test.index)
+			prediction.columns = ['forecast']
+			print(prediction)
+			return prediction
+
+	def update(df):
+		with open('LinearRegression.pkl', 'rb') as pkl:
+			linear_model = pickle.load(pkl).update(df)
+			with open('LinearRegression.pkl', 'wb') as pkl:
+				pickle.dump(linear_model, pkl)        
         
-        lin_pred=lin_model.predict(X_test)
-        print("Linear_Regression_Predictions")
-        test['Linear_Regression_Predictions']=lin_pred       
+    # def model(dataset):
+    #     # df=pd.DataFrame()
+    #     # df = pd.read_csv(dataset,index_col='Time',parse_dates=True)
+    #     df = dataset 
+    #     df.index = df['Time']
+    #     df.index.freq = 'MS'
+    #     #df.tail()
+    #     # df.columns = ['Usage']
+    #     df.plot(figsize=(12,8))
 
-        return test
+    #     df['Usage_LastMonth']=df['Usage'].shift(+1)
+    #     df['Usage_2Monthsback']=df['Usage'].shift(+2)
+    #     df['Usage_3Monthsback']=df['Usage'].shift(+3)
+    #     #df
+
+    #     df=df.dropna()
+    #     #df
+
+    #     from sklearn.linear_model import LinearRegression
+    #     lin_model=LinearRegression()
+
+    #     import numpy as np
+    #     x1,x2,x3,y=df['Usage_LastMonth'],df['Usage_2Monthsback'],df['Usage_3Monthsback'],df['Usage']
+    #     x1,x2,x3,y=np.array(x1),np.array(x2),np.array(x3),np.array(y)
+    #     x1,x2,x3,y=x1.reshape(-1,1),x2.reshape(-1,1),x3.reshape(-1,1),y.reshape(-1,1)
+    #     final_x=np.concatenate((x1,x2,x3),axis=1)
+
+    #     test_ind = len(df) - 30
+    #     train = df.iloc[:test_ind]
+    #     test = df.iloc[test_ind:]
+    #     X_train,X_test,y_train,y_test=final_x[:-30],final_x[-30:],y[:-30],y[-30:]
+
+    #     lin_model.fit(X_train,y_train)  
+        
+    #     lin_pred=lin_model.predict(X_test)
+    #     print("Linear_Regression_Predictions")
+    #     test['Linear_Regression_Predictions']=lin_pred       
+
+    #     return test
--- flask/xgb.py
+++ flask/xgb.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pickle
 
 class XGBClass:
     def __init__():
@@ -37,6 +38,20 @@ class XGBClass:
         X_train,X_test,y_train,y_test=final_x[:-30],final_x[-30:],y[:-30],y[-30:]
        
         xgb_model.fit(X_train,y_train)
+
+        # XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+        #       colsample_bynode=1, colsample_bytree=1, gamma=0, gpu_id=-1,
+        #       importance_type='gain', interaction_constraints='',
+        #       learning_rate=0.300000012, max_delta_step=0, max_depth=6,
+        #       min_child_weight=1, missing=nan, monotone_constraints='()',
+        #       n_estimators=100, n_jobs=0, num_parallel_tree=1,
+        #       objective='multi:softprob', random_state=0, reg_alpha=0,
+        #       reg_lambda=1, scale_pos_weight=None, subsample=1,
+        #       tree_method='exact', validate_parameters=1, verbosity=None)
+
+        pickle.dump(xgb_model, open('xgb.pkl', 'wb'))
+        pickled_model = pickle.load(open('xbg.pkl', 'rb'))
+        pickled_model.predict(X_test)
 
         xgb_pred=xgb_model.predict(X_test)
         print("Extreme_Gradient_Booster")
